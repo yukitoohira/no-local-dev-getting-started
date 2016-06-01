ae8526db502f79e056883f241354efea3535b44b
yukitoohira <yukito.ohira@f.flect.co.jp>
2016-06-01 17:01:28 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -11,7 +11,12 @@ end
 
 
 class Contact < ActiveRecord::Base
-  self.table_name = 'salesforce.contact'
+  self.table_name = 'salesforce.salesforce.username__c'
+end
+
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
 end
 
 #get "/contacts" do
